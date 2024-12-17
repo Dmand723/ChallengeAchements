18899da6f1a3028ebff3b617c53af225285c4f91
Dmand723 <dawson.simmons723@gmail.com>
2024-12-17 10:07:59 -0700

    NewStart

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-const express = require("express");
-const cors = require("cors");
-const mongodb = require("./db/connect");
+require("dotenv").config();
 
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const expressLayouts = require("express-ejs-layouts");
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+
+const methodOverride = require("method-override");
+const session = require("express-session");
+
+const connectDB = require("./server/config/db");
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
+app.use(expressLayouts);
+app.use(express.static("public"));
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-app
-  .use(cors())
-  .use(express.json())
-  .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-  })
-  .use("/", require("./routes"));
-mongodb.initDB((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(PORT);
-    console.log("\x1b[35m", `Connected to DB and listening on ${PORT}`);
-  }
-});
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+
+app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
