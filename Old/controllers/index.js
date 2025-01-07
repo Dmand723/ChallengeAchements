ce74c1810f4246a802a21d21ce7ea257d47c7708
Dmand723 <dawson.simmons723@gmail.com>
2025-01-07 08:20:04 -0700

    deleted old

--- Old/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const { mongo } = require("mongoose");
-const mongodb = require("../db/connect");
-const ObjectId = require("mongodb").ObjectId;
-
-const helloFunction = (req, res) => {
-  res.send("Hello from my route");
-};
-
-module.exports = { helloFunction };
